5c1258c3b664889988d56ac168f9ca51c181712d
Ezzat Chamudi <chamudi.git@outlook.com>
2019-11-07 23:09:07 +0300

    Add test

---
+++ test/test.js
@@ -0,0 +1,41 @@
+/* eslint-disable prefer-arrow-callback */
+
+const heyhoy = require('../');
+
+const codes = [
+        `
+        x = 5
+        y = x * 5
+
+        print y
+        `,
+
+        '',
+
+        `
+
+
+        `,
+
+        `
+        x1 = 20
+        y1 = 30
+        x2 = 30
+        y2 = 40
+
+        res = x1 * x2 + y1 * y2
+
+        print res
+        `,
+]
+
+describe('Work properly', function ()
+{
+        codes.forEach((code, index) =>
+        {
+                it(`passes test ${index}`, function ()
+                {
+                        heyhoy(code, true);
+                });
+        });
+});
